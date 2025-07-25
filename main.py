777ab642df52650188502e4888479db618a170c0
Vzhdanov95 <vgdanov@gmail.com>
2025-07-25 22:21:19 +0300

    23244-testing-commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# PEB 8 
+# Carbage collector 
+# Mutable
+some_list = [2, 4, 5, 40]
+some_set = {"Audi", "Toyota", "Audi"}
+some_dict = {"car_1": "Chevrolet", "car_2": "Hundai"}
+
+some_set.add("Garage")
+# print(some_set)
+
+# Immutable
+
+not_change = frozenset(("Audi", "Toyota", "BMW"))
+
+some_tuple = (1, 1, 3, 4, 5, 6)
+# print(some_tuple)
+
+friends = ["John", "Gordon", "Vasiliy"]
+
+for friend in friends:
+    print(friend)
+
+result = 40
+while True:
+    print(40)
+    result = 42
+    break
+print(result)
+    
